07b1dbabbb59e4cada066512730ed83a467a5d26
Tim Golsby-Smith <tim.gs@secondroad.com.au>
2014-01-04 09:20:25 +1100

    Dynamic home page

---
+++ app/models/owner.rb
@@ -0,0 +1,21 @@
+class Owner
+
+	def name
+		name = 'Tommy Golsby-Smith'
+	end
+
+	def birthdate
+		birthdate = Date.new(2002, 02, 18)
+	end
+
+	def countdown
+		today = Date.today
+		birthday = Date.new(today.year, birthdate.month, birthdate.day)
+		if birthday > today
+			countdown = (birthday - today).to_i
+		else
+			countdown = (birthday.next_year - today).to_i
+		end
+	end
+
+end
